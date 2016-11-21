e1bbe004a22ae0cf2097f7def4e5ddb36241e7ea
Matheus Gaudencio do Rêgo <matheusgr@gmail.com>
2016-11-20 22:37:18 -0300

    Adicionado o exemplo de controle acadêmico.

---
+++ src/aula5/controle_academico_respostas/ControleAcademico.java
@@ -0,0 +1,55 @@
+package aula5.controle_academico_respostas;
+public class ControleAcademico {
+
+	private Aluno[] alunos;
+	private Turma[] turmas;
+	private int alunosCadastrados;
+	private int turmasCadastradas;
+
+	public ControleAcademico() {
+		this.alunos = new Aluno[30000];
+		this.alunosCadastrados = 0;
+		this.turmas = new Turma[3000];
+		this.turmasCadastradas = 0;
+	}
+
+	public void cadastraAluno(String nome, int matricula) {
+		this.alunos[alunosCadastrados] = new Aluno(nome, matricula);
+		alunosCadastrados += 1;
+	}
+
+	public void cadastraAluno(String nome, int matricula, String apelido) {
+		this.alunos[alunosCadastrados] = new Aluno(nome, matricula, apelido);
+		alunosCadastrados += 1;
+	}
+
+	public void cadastraTurma(String nome) {
+		this.turmas[turmasCadastradas] = new Turma(nome);
+		turmasCadastradas += 1;
+	}
+
+	private Turma consultaTurma(String nome) {
+		for (int i = 0; i < turmasCadastradas; i++) {
+			if (turmas[i].getNome().equals(nome)) {
+				return turmas[i];
+			}
+		}
+		return null;
+	}
+
+	private Aluno consultaAluno(int matricula) {
+		for (int i = 0; i < alunosCadastrados; i++) {
+			if (alunos[i].getMatricula() == matricula) {
+				return alunos[i];
+			}
+		}
+		return null;
+	}
+
+	public void cadastraAlunoTurma(int matriculaAluno, String nome) {
+		Turma turma = consultaTurma(nome);
+		Aluno aluno = consultaAluno(matriculaAluno);
+		turma.cadastraAluno(aluno);
+	}
+
+}
---
+++ src/aula5/controle_academico_respostas/Turma.java
@@ -0,0 +1,23 @@
+package aula5.controle_academico_respostas;
+public class Turma {
+
+	private Aluno[] alunos;
+	private String nome;
+	private int alunosCadastrados;
+
+	public Turma(String nome) {
+		this.nome = nome;
+		this.alunos = new Aluno[1000];
+		this.alunosCadastrados = 0;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void cadastraAluno(Aluno aluno) {
+		this.alunos[alunosCadastrados] = aluno;
+		this.alunosCadastrados += 1;
+	}
+
+}
