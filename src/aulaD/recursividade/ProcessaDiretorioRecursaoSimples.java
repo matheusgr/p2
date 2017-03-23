e90623afe8b5ee2d0e7f7721251fa6b369d22269
Matheus Gaudencio do Rêgo <matheusgr@gmail.com>
2017-03-23 01:09:50 -0300

    Organizando imports.

--- src/aulaD/recursividade/ProcessaDiretorioRecursaoSimples.java
+++ src/aulaD/recursividade/ProcessaDiretorioRecursaoSimples.java
@@ -2,7 +2,6 @@ package aulaD.recursividade;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
