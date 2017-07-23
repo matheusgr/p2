3011f0a9662feb55cd2e2fe3c0da7cc3b8dabaaf
Matheus Gaudencio do Rêgo <matheusgr@gmail.com>
2017-07-23 20:38:16 -0300

    Adicionando exemplos de interfaces.

---
+++ src/interfaces/BancoDeDados.java
@@ -0,0 +1,11 @@
+package interfaces;
+import java.util.Map;
+
+public class BancoDeDados {
+
+	public void salvar(Map<Integer, Contabil> contas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/interfaces/Poupanca.java
@@ -0,0 +1,25 @@
+package interfaces;
+public class Poupanca extends Conta {
+
+	public Poupanca(String cpf, int quantia) {
+		super(cpf, quantia);
+	}
+
+	 public int sacar(int quantia) {
+		this.quantia -= quantia;
+		return quantia;
+	}
+	
+	@Override
+	public String getTipo() {
+		return "Poupanca";
+	}
+
+	@Override
+	public String getDetalhes() {
+		return ".";
+	}
+
+	
+	
+}
