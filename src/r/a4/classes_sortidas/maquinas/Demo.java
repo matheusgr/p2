300c37cd2ba7b3853391202edb47cd0688a4346c
matheusgr <matheusgr@bricolage>
2018-10-19 14:09:05 -0300

    minor fix.

--- src/r/a4/classes_sortidas/maquinas/Demo.java
+++ src/r/a4/classes_sortidas/maquinas/Demo.java
@@ -6,7 +6,6 @@ public class Demo {
 		Aluno a1 = new Aluno("Matheus", "1234");
 		Aluno a2 = new Aluno("Gabriel", "6789");
 		a1.equals(null);
-		a1.equals("abc");
 		System.out.println(a1.equals(a2));
 	}
 	
