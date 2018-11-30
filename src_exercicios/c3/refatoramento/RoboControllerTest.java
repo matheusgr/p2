8fc7fc86242bed1d624ae348fa5a8d869b9320ef
matheusgr <matheusgr@gmail.com>
2018-11-30 08:44:47 -0300

    JUnit 4.

--- src_exercicios/c3/refatoramento/RoboControllerTest.java
+++ src_exercicios/c3/refatoramento/RoboControllerTest.java
@@ -2,14 +2,14 @@ package c3.refatoramento;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 public class RoboControllerTest {
 
 	private RoboController roboController;
 
-	@BeforeEach
+	@Before
 	public void prepara() {
 		this.roboController = new RoboController();
 		this.roboController.criarRobo("abc", 20);
